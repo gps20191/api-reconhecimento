c0d7143950e3eb13b342ff008703ffd169e7b8dc
elimarmacena <elimar.macena@gmail.com>
2019-06-10 16:33:07 -0300

    UPDATE: alteracao no script do banco e alteracao na imagem de parametros recebidos via api

--- DATABASE/SQL/sql_create.sql
+++ DATABASE/SQL/sql_create.sql
@@ -1,27 +1,13 @@
-CREATE TABLE AlertRequest (
+CREATE TABLE AlertRequest(
     RequestId SERIAL PRIMARY KEY,
     Processed BIT,
     Match BIT,
-    urlPhoto VARCHAR,
-    Location REAL ARRAY[2],
-    RequestDate DATE
-);
-
-CREATE TABLE AlertNotified (
-    NotifiedId SERIAL PRIMARY KEY,
-    NotificationDate DATE,
-    RequestId INTEGER REFERENCES AlertRequest(RequestId)
-);
-
-CREATE TABLE AlertArchive (
-    ArchiveId SERIAL PRIMARY KEY,
-    ArchiveDate DATE,
-    RequestId INTEGER REFERENCES AlertRequest(RequestId)
-);
-
-CREATE TABLE Procurados (
-    ProcuradoId SERIAL PRIMARY KEY,
-    FaceData REAL ARRAY,
-    Nome VARCHAR
-);
- 
+    Alerted BIT,
+    UrlPhoto VARCHAR,
+    BlobImg VARCHAR,
+    Latitude REAL,
+    Longitude REAL,
+    ResquestDate DATE,
+    IdPhoto INTEGER,
+    NumBus INTEGER
+)
